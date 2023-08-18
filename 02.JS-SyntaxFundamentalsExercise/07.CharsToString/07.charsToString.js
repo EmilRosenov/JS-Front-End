32069f5f6687b94161c5bb13e12a07681c3dcaf3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2023-08-18 23:15:03 +0100

    Add files via upload

---
+++ 02.JS-SyntaxFundamentalsExercise/07.CharsToString/07.charsToString.js
@@ -0,0 +1,15 @@
+function createString(charOne,charTwo,charTree){
+let arr = [];
+arr.push(charOne);
+arr.push(charTwo);
+arr.push(charTree);
+console.log(arr.join(''));
+}
+
+
+createString('a','b','c');
+createString('%','2','o');
+createString('1',
+'5',
+'p'
+);
