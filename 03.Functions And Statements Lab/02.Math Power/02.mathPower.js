48999743b54ebb82c06f8997876b16fd15107f16
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2023-08-25 23:16:31 +0100

    Add files via upload

---
+++ 03.Functions And Statements Lab/02.Math Power/02.mathPower.js
@@ -0,0 +1,9 @@
+function printResult(number, power) {
+  function result(number, power) {
+    console.log(Number(Math.pow(number, power)));
+  }
+  result(number, power);
+}
+
+printResult(2, 8);
+printResult(3, 4);
