0ba5cc2f0d539c65a4b681ebd9e2f0d13ced6d8d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2023-08-29 16:44:20 +0100

    Add files via upload

---
+++ 04.FuntionsAndStatementExercise/01.Smallest of Three Numbers/01.smallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function findSmallestNumber(numOne, numTwo, numThree) {
+  const smallest = Math.min(numOne, numTwo, numThree);
+  return console.log(smallest);
+}
+
+findSmallestNumber(2, 5, 3);
+findSmallestNumber(600, 342, 123);
+findSmallestNumber(25, 21, 4);
+findSmallestNumber(2, 2, 2);
