d11bbba6937996feaf88a518e2e9c67d4d7ce2d5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2023-08-29 16:44:40 +0100

    Add files via upload

---
+++ 04.FuntionsAndStatementExercise/02.AddAndSubtract/02.addAndSubtract.js
@@ -0,0 +1,9 @@
+function solve(firstNum, secondNum, thirdNum) {
+  const result = (firstNum, secondNum, thirdNum) =>
+    firstNum + secondNum - thirdNum;
+  console.log(result(firstNum, secondNum, thirdNum));
+}
+
+solve(23, 6, 10);
+solve(1, 17, 30);
+solve(42, 58, 100);
